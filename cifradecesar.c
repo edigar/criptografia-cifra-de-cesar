a314c1dafcb29f16a639be7db8696d2cbf3813a9
edigarhdev <edigarhdev@gmail.com>
2019-06-06 16:37:00 -0300

    Adiciona loop de permanência no programa e opção de sair

--- cifradecesar.c
+++ cifradecesar.c
@@ -11,51 +11,56 @@ void main() {
 
     setlocale(LC_ALL, "Portuguese");
 
-    printf("Escolha:\n\n1. Criptografar\n2. Decriptar\n\nOpção: ");
-    scanf("%d", &option);
+    do {
+        printf("Escolha:\n\n1. Criptografar\n2. Decriptar\n3. Sair\n\nOpção: ");
+        scanf("%d", &option);
 
-    if(option == 1 || option == 2) {
-        printf("\n\n\n\n***********************************************\n*         CRIPTOGRAFIA CIFRA DE CESAR         *\n***********************************************\n\n");
-        if(option == 1) {
-            strcpy(type, "criptografado");
-            printf("\nInstruções:\n\n* O Arquivo contendo o texto a ser criptografado deve ser um tipo txt. \n* A chave (criptográfica) é um número de X a Y???\n\n");
-        } else if(option== 2) {
-            strcpy(type, "decriptado");
-            printf("\nInstruções:\n\n* O Arquivo contendo o texto a ser decriptado deve ser um tipo txt. \n* A chave (criptográfica) deve ser a mesma que foi utilizada para criptografar\n\n");
-        }
-
-        printf("Nome do arquivo (txt) a ser %s: ", type);
-        scanf("%s", name);
-        strcpy(file_name, name);
-        strcat(file_name, ".txt");
-
-        file_enter = fopen(file_name, "r");
-        if(file_enter == NULL) {
-            printf("Falha ao abrir arquivo a ser %s (%s)\n\n\n", type, file_name);
-        } else {
-            strcpy(file_name, name);
-            strcat(file_name, "_");
-            strcat(file_name, type);
-            strcat(file_name, ".txt");
-            file_cript = fopen(file_name, "w");
-            if(file_cript == NULL) {
-                printf("Falha ao abrir arquivo de destino\n\n\n");
-            } else {
-                printf("Chave: ");
-                scanf("%d", &key);
-                key = option == 2 ? key * -1 : key;
-                while((character = fgetc(file_enter)) != EOF) {
-                    charcript = character + key;
-                    fputc(charcript, file_cript);
-                }
-
-                printf("\nArquivo %s em %s\n\n\n", type, file_name);
-
-                fclose(file_enter);
-                fclose(file_cript);
+        if(option == 1 || option == 2) {
+            printf("\n\n\n\n***********************************************\n*         CRIPTOGRAFIA CIFRA DE CESAR         *\n***********************************************\n\n");
+            if(option == 1) {
+                strcpy(type, "criptografado");
+                printf("\nInstruções:\n\n* O Arquivo contendo o texto a ser criptografado deve ser um tipo txt. \n* A chave (criptográfica) é um número de X a Y???\n\n");
+            } else if(option== 2) {
+                strcpy(type, "decriptado");
+                printf("\nInstruções:\n\n* O Arquivo contendo o texto a ser decriptado deve ser um tipo txt. \n* A chave (criptográfica) deve ser a mesma que foi utilizada para criptografar\n\n");
             }
+
+            printf("Nome do arquivo (txt) a ser %s: ", type);
+            scanf("%s", name);
+            strcpy(file_name, name);
+            strcat(file_name, ".txt");
+
+            file_enter = fopen(file_name, "r");
+            if(file_enter == NULL) {
+                printf("Falha ao abrir arquivo a ser %s (%s)\n\n\n", type, file_name);
+            } else {
+                strcpy(file_name, name);
+                strcat(file_name, "_");
+                strcat(file_name, type);
+                strcat(file_name, ".txt");
+                file_cript = fopen(file_name, "w");
+                if(file_cript == NULL) {
+                    printf("Falha ao abrir arquivo de destino\n\n\n");
+                } else {
+                    printf("Chave: ");
+                    scanf("%d", &key);
+                    key = option == 2 ? key * -1 : key;
+                    while((character = fgetc(file_enter)) != EOF) {
+                        charcript = character + key;
+                        fputc(charcript, file_cript);
+                    }
+
+                    printf("\nArquivo %s em %s\n\n\n", type, file_name);
+
+                    fclose(file_enter);
+                    fclose(file_cript);
+                }
+            }
+        } else if(option == 3) {
+            printf("\n\nTenha um bom dia.\nTchau!\n");
+        } else {
+            printf("\n\nOpção inválida!\n\n");
         }
-    } else {
-        printf("\n\nOpção inválida!\n\n");
-    }
+
+    } while(option != 3);
 }
